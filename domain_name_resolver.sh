33a226680b33c1a006fb8d679f565746e123f001
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2023-08-12 20:56:55 +0530

    Updated

--- domain_name_resolver.sh
+++ domain_name_resolver.sh
@@ -2,6 +2,7 @@
 file_path="domain_names.txt"
 
 while IFS= read -r domain_name; do
+    domain_name=$(echo "$domain_name" | tr -d '[:space:]')
     first_char="${domain_name%"${domain_name#?}"}"
     if [ "$first_char" != "#" ]; then
         result=$(dig "$domain_name" @1.1.1.1 | grep ";; ANSWER SECTION:")
@@ -10,8 +11,8 @@ while IFS= read -r domain_name; do
         else
             output="Results"
         fi
+        echo "$domain_name   $output"
     fi
-    echo "$domain_name"
 done < "$file_path"
 
 #Hello
