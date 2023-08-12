f5a8b8949554bddc76fecb4051f30505b132af9e
Pasindu Bhagya <pasindubhagyapeiries@gmail.com>
2023-08-12 06:45:51 +0530

    Updated

--- domain_name_resolver.sh
+++ domain_name_resolver.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+file_path="domain_names.txt"
+
+while IFS= read -r domain_name; do
+    if [ "${domain_name:0:1}" != "#" ]; then
+        dig "$domain_name" @1.1.1.1 | grep ";; ANSWER SECTION:"
+    fi
+done < "$file_path"
