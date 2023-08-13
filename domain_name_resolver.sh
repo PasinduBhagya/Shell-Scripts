bf9a6667dbc5d600aa6eeee075a3da0b45c2154f
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2023-08-13 15:25:46 +0530

    Completed

--- domain_name_resolver.sh
+++ domain_name_resolver.sh
@@ -3,10 +3,11 @@ file_path="domain_names.txt"
 
 while IFS= read -r domain_name; do
     domain_name=$(echo "$domain_name" | tr -d '[:space:]')
-    domain_name="dns.google.com"
     first_char="${domain_name%"${domain_name#?}"}"
     if [ "$first_char" != "#" ]; then
-        printf "%-25s" "$domain_name"
+        echo "====================================================="
+        printf "%-25s\n" "$domain_name"
+        echo "-----------------------------------------------------"
 
         result=$(dig "$domain_name" @1.1.1.1 | grep ";; ANSWER SECTION:")
         
@@ -14,14 +15,11 @@ while IFS= read -r domain_name; do
             output="No Results"
         else
             output=$(dig "$domain_name" @1.1.1.1 | awk '/;; ANSWER SECTION:/ { flag=1; next } flag { if (NF == 0) exit; print $4, $5 }')
-            line_count=$(echo "$output" | wc -l)
         fi
 
-        count=1
-        while [ $count -lt $line_count ]; do
-            printf "%-25s" "$output"
-            ((count++))
-        done
+        printf "%-25s\n" "$output"
+        echo "====================================================="
+
         
     fi
 done < "$file_path"
